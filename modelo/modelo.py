1b172ef9c7b57c9381a55adad6768a3934b643bd
Rafael Faustini <contato@rafaelfaustini.com.br>
2020-09-04 10:55:08 -0300

    K-Fold (Validação Cruzada) K=10

--- modelo/modelo.py
+++ modelo/modelo.py
@@ -6,6 +6,8 @@ import joblib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.model_selection import GridSearchCV
 
 df = pd.read_pickle("dados/dataset-treino.df")
 df_treinamento = df
@@ -21,30 +23,45 @@ for i in range(n, len(df)):
     stress.append(df[i,3])
 previsores, stress = np.array(previsores).astype('float64'), np.array(stress)
 
-model = Sequential()
-model.add(LSTM(units = 100, return_sequences = True, input_shape = (79, 2)))
-model.add(Dropout(0.3))
-
-model.add(LSTM(units = 50, return_sequences = True))
-model.add(Dropout(0.3))
-
-model.add(LSTM(units = 50, return_sequences = True))
-model.add(Dropout(0.3))
-
-model.add(LSTM(units = 50))
-model.add(Dropout(0.3))
-
-model.add(Dense(units = 1, activation = 'sigmoid'))
-
-model.compile(optimizer = 'adam', loss = 'mean_squared_error',
-                  metrics = ['mean_absolute_error'])
+def criarModelo (optimizer, loss, kernel_initializer, neurons, dropout):
+    model = Sequential()
+    model.add(LSTM(units = 150, return_sequences = True, input_shape = (79, 2)))
+    model.add(Dropout(dropout))
+    
+    model.add(LSTM(units = neurons, return_sequences = True))
+    model.add(Dropout(dropout))
+    
+    model.add(LSTM(units = neurons, return_sequences = True))
+    model.add(Dropout(dropout))
+    
+    model.add(LSTM(units = neurons))
+    model.add(Dropout(dropout))
+    
+    model.add(Dense(units = 1, activation = 'sigmoid'))
+    
+    model.compile(optimizer = optimizer, loss = loss,
+                      metrics = ['mean_absolute_error'])
+    return model
 
 es = EarlyStopping(monitor='loss', min_delta = 1e-10, patience = 10, verbose=1)
 rlr = ReduceLROnPlateau(monitor='loss', factor=0.2, patience = 5, verbose=1)
 checkpoint = ModelCheckpoint(filepath='checkpoints/pesos.h5', monitor='loss',
                              save_best_only=True)
-model.fit(previsores, stress, epochs = 100, batch_size = 32,
-          callbacks=[es,rlr, checkpoint])
+model = KerasRegressor(build_fn=criarModelo)
+parametros = {
+    'batch_size': [32, 64],
+    'epochs': [100],
+    'optimizer': ['adam', 'sgd'],
+    'loss': ['mean_absolute_error', 'binary_crossentropy'],
+    'neurons': [50, 100],
+    'dropout': [0.3, 0.5]
+}
+
+grid_search = GridSearchCV(estimator=model, param_grid=parametros, scoring='neg_mean_absolute_error', cv=10)
+grid_search = grid_search.fit(previsores, stress, callbacks=[es,rlr, checkpoint])
+melhores_parametros = grid_search.best_params_
+melhor_precisao = grid_search.best_score_
+
 
 df_teste = pd.read_pickle("dados/dataset-teste.df")
 stress_teste = df_teste.iloc[:, 3].values
