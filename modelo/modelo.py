306cbbcfe3db84a367b1095655cec2c300ff5ad8
Rafael Faustini <contato@rafaelfaustini.com.br>
2020-10-02 22:39:15 -0300

    Organização + Começo Tunning

--- modelo/modelo.py
+++ modelo/modelo.py
@@ -1,37 +1,21 @@
 from keras.models import Sequential
-from keras.models import model_from_json
-from keras.layers import Dense, Dropout, LSTM, BatchNormalization, Flatten, TimeDistributed
+from keras.layers import Dense, Dropout
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint, TensorBoard
-import joblib
-import pandas as pd
+from tensorflow.keras.optimizers import SGD
 import numpy as np
 import matplotlib.pyplot as plt
-from keras.wrappers.scikit_learn import KerasClassifier
-from sklearn.model_selection import TimeSeriesSplit, StratifiedKFold
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import StratifiedKFold
 import difflib
 import time
-import matplotlib.pyplot as plt
 
 # Configurações
-n = 7 # Numero de horas anteriores para serem analisados
 k = 10 # Número de folds no K-fold Validation
+##
 
+# Dados de treino
 x = np.load("dados/x_treino.npy")
 y = np.load("dados/y_treino.npy")
-
-
-# Loop que vai formar os dados de treino multidimensionais para as redes LSTM
-# com base no timestep n
-def loopAnteriores(x, y):
-    p = []
-    s = []
-    for i in range(n, len(x)):
-        p.append(x[i-n:i, :])
-        s.append(y[i])  
-    p,s = np.array(p).astype("float32"), np.array(s).astype("int32")
-    return p,s
+##
 
 # Função que plota um gráfico comparativo entre as classes reais e previstas
 def plotarComparacoes(model, x_teste, y_teste, estatisticas=1):
@@ -59,17 +43,14 @@ def plotarComparacoes(model, x_teste, y_teste, estatisticas=1):
 
 def criarModelo ():
     model = Sequential()
-    model.add(Flatten(input_shape=(73, 2)))
-    model.add(Dense(units = 128, activation='relu'))
-    model.add(Dropout(0.5))
+    model.add(Dense(units=73, activation='relu', kernel_initializer='random_uniform', input_dim = 73))
+    model.add(Dropout(0.6))
     
-    for j in range(0, 2):
-        model.add(Dense(units = 64, activation = 'relu'))
-        model.add(Dropout(0.75))
+    model.add(Dense(units = 128, activation = 'relu'))
 
     model.add(Dense(units = 1, activation = 'sigmoid'))
-    
-    model.compile(optimizer = 'adam', loss = 'binary_crossentropy',
+    otimizador = SGD(momentum=0.9, learning_rate=0.005)
+    model.compile(optimizer = otimizador, loss = 'binary_crossentropy',
                       metrics = ['accuracy'])
     return model
 
@@ -77,37 +58,36 @@ def criarModelo ():
 # Dataset teste
 x_teste = np.load("dados/x_teste.npy")
 y_teste = np.load("dados/y_teste.npy")
-
-#x_teste, y_teste = loopAnteriores(x_teste, y_teste)
+##
 
 # Callbacks
-es = EarlyStopping(monitor='loss', min_delta = 1e-10, patience = 20, verbose=1)
-rlr = ReduceLROnPlateau(monitor='loss', factor=0.2, patience = 3, verbose=0)
+es = EarlyStopping(monitor='val_loss', min_delta = 1e-10, patience = 20, verbose=1)
+rlr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience = 3, verbose=0)
 checkpoint = ModelCheckpoint(filepath='checkpoints/pesos.h5', monitor='loss',
                              save_best_only=True)
+##
 
 
 model = criarModelo()
 kfold = StratifiedKFold(n_splits=k, shuffle=False)
 cvscores = []
 
+# K-fold
 for treino, teste in kfold.split(x, y):
-    history = model.fit(x[treino], y[treino], epochs=550, batch_size=128, verbose=0, callbacks=[es, rlr])
+    history = model.fit(x[treino], y[treino], epochs=550, batch_size=20, verbose=0)
     scores = model.evaluate(x[teste], y[teste], verbose=0)
-    print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+    print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100)) # Resultado do Fold
     cvscores.append(scores[1] * 100)
-print("%.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
+print("%.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores))) # Resultado Final
+##
 
+# Treino Normal (Sem Kfold)
 NOME = "Smartcare-Stress-Idoso-ANN-%s"%(int(time.time()))
 tensorboard = TensorBoard(log_dir='logs/{}'.format(NOME))
-h = model.fit(x,y, validation_split=0.2, epochs=550, batch_size=16, callbacks=[tensorboard, es, rlr])
-    
-#print(results.mean())
+h = model.fit(x,y, validation_split=0.2, epochs=500, batch_size=20)
+##
 
-#model.fit(x = x, y = y, batch_size = 128, epochs=500, 
-#              callbacks=[es, rlr,tensorboard, checkpoint], validation_split=0.33,
-#              verbose=0)
-
+# Gráfico Treino Normal
 plt.plot(h.history['accuracy'])
 plt.plot(h.history['val_accuracy'])
 plt.title('AcurÃ¡cia do modelo')
@@ -122,6 +102,8 @@ plt.ylabel('perda')
 plt.xlabel('Ã©poca')
 plt.legend(['train', 'test'], loc='best')
 plt.show()
+##
 
+# Plot comparação dataset teste previsão
 plotarComparacoes(model, x_teste, y_teste)
-#"""
+##
